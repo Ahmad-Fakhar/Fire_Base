3c67ce5eac7124888b701c7383b6937591614a30
Ahmad Fakhar <155258276+Ahmad-Fakhar@users.noreply.github.com>
2024-03-01 08:18:50 +0500

    Initial commit

---
+++ app/src/main/java/com/example/firebase/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.firebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @SuppressLint({"MissingInflatedId", "SetTextI18n"})
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // initialising all views through id defined above
+        TextView forgetfulness = findViewById(R.id.login);
+        forgetfulness.setText(
+                "(Firebase Authentication)");
+    }
+}
---
+++ app/src/main/java/com/example/firebase/MainActivity2.java
@@ -0,0 +1,34 @@
+package com.example.firebase;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import com.example.firebase.RegistrationActivity;
+
+public class MainActivity2 extends AppCompatActivity {
+    private static final int SPLASH_SCREEN_TIME_OUT = 2000; // After completion of 4000 ms, the next activity will get started.
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // This method is used so that your splash activity can cover the entire screen.
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main); // this will bind your MainActivity.class file with activity_main.
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Intent is used to switch from one activity to another.
+                Intent i = new Intent(MainActivity2.this, RegistrationActivity.class);
+                startActivity(i); // invoke the SecondActivity.
+                finish(); // the current activity will get finished.
+            }
+        }, SPLASH_SCREEN_TIME_OUT);
+    }
+}
